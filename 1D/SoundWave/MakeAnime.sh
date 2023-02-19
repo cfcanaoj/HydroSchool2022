db64d5db8323bab9d930c20c73c550d2fda2b076
kazunari-iwasaki <kazunari.iwasaki@gmail.com>
2023-02-20 00:46:45 +0900

    modify to output soundwave

--- 1D/SoundWave/MakeAnime.sh
+++ 1D/SoundWave/MakeAnime.sh
@@ -1,32 +1,55 @@
 #!/bin/bash
 
-dirf=snap
-dirm=snap
+#declare -i nmin nmax
+
+dir=$1
+nmin=$2
+nmax=$3
 prename=den
 
-if [ ! -d $dirf ]; then
-    echo "cannot find ",$dirf
+if [ ! -d $dir ]; then
+    echo "cannot find ",$dir
     exit
 fi
 
-for n in {1..40} ;do
-    gnuplot -e num=$n MakePngFile.plt
-#    gnuplot -e num=$n x-p.plt
-#    gnuplot -e num=$n x-v.plt
+for n in `seq $nmin $nmax` ;do
+gnuplot <<EOF
+set terminal push
+set terminal png
+
+set xlabel "x"
+
+#set yrange [-1.1e-5:1.1e-5]
+set xrange [-0.5:0.5]
+
+inpfile=sprintf("$dir/snap%05d.dat",$n)
+outfile=sprintf("$dir/den%05d.png",$n)
+
+# extract time from the first line
+command=sprintf("awk 'NR==1{print(\$2)}' %s", inpfile)
+time=system(command)
+set output outfile
+set title sprintf("time = %s",time) 
+
+pi = acos(-1.0)
+cs = 1.0
+plot inpfile u 1:(\$2-1) ti "numerical" w lp, 1e-5*sin(2.0*pi*(x-cs*time)) ti "exact solution" w l
+
+print inpfile
+
+set terminal pop
+EOF
 done
 
-if [ ! -d $dirm ]; then
-    mkdir $dirm
-fi
-
 # first file
-fstfile=`ls -1 ${dirf}/${prename}*.png  2>/dev/null | head -1`
+fstfile=`ls -1 ${dir}/${prename}*.png  2>/dev/null | head -1`
 declare -i fstnum=`echo  ${fstfile##*/} | tr -cd '0123456789\n' |sed -e 's/^0\+\([0-9]\+\)$/\1/'`
 echo $fstnum
 
-ffmpeg -y -r 10  -start_number ${fstnum} -i ${dirf}/${prename}%5d.png -b 6000k -vcodec wmv2 -pass 1 -r 10 -an ${dirm}/ani${prename}.wmv
+echo "wmv format"
+ffmpeg -y -r 10  -start_number ${fstnum} -i ${dir}/${prename}%5d.png -b 6000k -vcodec wmv2 -pass 1 -r 10 -an ${dir}/animate.wmv
 
 echo "mp4 format"
-ffmpeg -y -r 10  -start_number ${fstnum} -i ${dirf}/${prename}%5d.png -vcodec libx264 -pix_fmt yuv420p -r 10 -an ${dirm}/ani${prename}.mp4
+ffmpeg -y -r 10  -start_number ${fstnum} -i ${dir}/${prename}%5d.png -vcodec libx264 -pix_fmt yuv420p -r 10 -an ${dir}/animate.mp4
 
 exit
