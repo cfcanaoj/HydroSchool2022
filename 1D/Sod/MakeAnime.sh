d3ac669157847e417c8ad970c9a9c12c7cfb860b
kazunari-iwasaki <kazunari.iwasaki@gmail.com>
2023-02-19 23:18:31 +0900

    modify so that plt file is not used

--- 1D/Sod/MakeAnime.sh
+++ 1D/Sod/MakeAnime.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-dir=lax
+#declare -i nmin nmax
+
+dir=$1
+nmin=$2
+nmax=$3
 prename=den
 
 if [ ! -d $dir ]; then
@@ -8,8 +12,29 @@ if [ ! -d $dir ]; then
     exit
 fi
 
-for n in {1..40} ;do
-    gnuplot -e num=$n MakePngFile.plt
+for n in `seq $nmin $nmax` ;do
+gnuplot <<EOF
+set terminal push
+set terminal png
+
+set xlabel "x"
+
+set yrange [0.0:1.2]
+set xrange [-0.5:0.5]
+
+inpfile=sprintf("$dir/snap%05d.dat",$n)
+outfile=sprintf("$dir/den%05d.png",$n)
+
+# extract time from the first line
+command=sprintf("awk 'NR==1{print(\$2)}' %s", inpfile)
+time=system(command)
+set output outfile
+
+set title sprintf("time = %s",time) 
+plot inpfile u 1:2 ti "numerical" w lp, "sod_ana.dat" u (\$1*time/0.2):2 ti "exact solution" w l
+
+set terminal pop
+EOF
 done
 
 # first file
