82f270198f8ca056f97f5ff1d05504c83ed3d1fd
kazunari-iwasaki <kazunari.iwasaki@gmail.com>
2023-02-22 09:45:36 +0900

    modify

--- multiD/BlastWave/kick.sh
+++ multiD/BlastWave/kick.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#PBS -q single
+#PBS -q small
 ##PBS -m abe
 ##PBS -M (メールアドレス)
-#PBS -l nodes=1:ppn=8
+#PBS -l nodes=1:ppn=4
 #PBS -l walltime=00:30:00
 
 cd $PBS_O_WORKDIR
 
-export OMP_NUM_THREADS=8
+export OMP_NUM_THREADS=4
 
-logfile=$(date +log.%Y-%m-%d-%H:%M:%S)
+logfile=$(date +log.%Y-%m-%d-%H%M%S)
 
 ./a.out >& $logfile
